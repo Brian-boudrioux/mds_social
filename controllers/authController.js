0c227bbc9071abdd7e9c1fc935a8c59c8399a933
Brian Boudrioux <bboudrioux@gmail.com>
2025-12-17 10:36:18 +0100

    resolve error

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,6 +31,22 @@ const login = async (req, res, next) => {
     }
 }
 
+const register = async (req, res, next) => {
+  try {
+    const { pseudo, email, password } = req.body;
+    if (!pseudo || !email || !password) return res.status(400).json("all field is required");
+
+    const [[user]] = await authModel.findUserByEmail(email);
+    if (user) return res.status(400).json("user with specified email already exist");
+
+    const hashedPassword = await argon2.hash(password);
+    const [result] = await authModel.createUser(pseudo, email, hashedPassword);
+    if (result.affectedRows > 0) res.status(201).json({ id: result.insertId, pseudo, email });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export default {
     register,
     login,
